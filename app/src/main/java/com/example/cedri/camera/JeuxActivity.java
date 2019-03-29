648a155bf2950f2da41f59386cb5fc969d41eb87
Cedric-Duraysseix <cedric.duraysseix@etu.unilim.fr>
2019-03-29 20:18:20 +0100

    enlevement du code inutile

--- app/src/main/java/com/example/cedri/camera/JeuxActivity.java
+++ app/src/main/java/com/example/cedri/camera/JeuxActivity.java
@@ -1,44 +1,31 @@
 package com.example.cedri.camera;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.annotation.RequiresApi;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class JeuxActivity extends AppCompatActivity {
     private PaintView paintView;
     private Drawable photo;
     private ImageView upload;
+    private int cpt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,37 +44,33 @@ public class JeuxActivity extends AppCompatActivity {
         paintView.init(metrics);
 
         upload = (ImageView) findViewById(R.id.upload);
-
+        cpt = 1;
         upload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Bitmap capturedBitmap = Screenshot.takescreenshotOfRootView(findViewById(R.id.jeuxActivity)); //screenshot est nul
-                savePicture(capturedBitmap,"Screenshot");
-                MediaStore.Images.Media.insertImage(getContentResolver(),capturedBitmap,"screen","description");
+                Bitmap capturedBitmap = Screenshot.takescreenshotOfRootView(findViewById(R.id.jeuxActivity));
+                File file = new File(getApplicationContext().getExternalFilesDir(
+                        Environment.DIRECTORY_DOWNLOADS), "GALERYE");
+                if (!file.mkdirs()) {
+                    Log.e("1", "Directory not created");
+                }
+                File image = new File(file, "image" + cpt + ".jpg");
+                cpt++;
+                System.out.print(image.getAbsolutePath());
+                try{
+
+                    FileOutputStream img = new FileOutputStream(image);
+
+                    capturedBitmap.compress(Bitmap.CompressFormat.PNG, 100, img);
+                    img.close();
+
+
+                    System.out.println("File Saved");
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
             }
         });
-    }
-
-    private void savePicture(Bitmap bm, String imgName)
-    {
-        OutputStream fOut = null;
-        String strDirectory = Environment.getExternalStorageDirectory().toString();
-
-        File f = new File(strDirectory, imgName);
-        try {
-            fOut = new FileOutputStream(f);
-
-            /**Compress image**/
-            bm.compress(Bitmap.CompressFormat.JPEG, 85, fOut);
-            fOut.flush();
-            fOut.close();
-
-            /**Update image to gallery**/
-            MediaStore.Images.Media.insertImage(getContentResolver(),
-                    f.getAbsolutePath(), f.getName(), f.getName());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     public static void setBackground(View v, Drawable drawable){
